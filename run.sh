d2d85fcefd0c0f9a4ad0d6ee610001217aee6e02
J.R. Patten <jr@juanpatten.com>
2013-11-19 09:14:16 -0700

    config fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/opt/elasticsearch/bin/plugin -install mobz/elasticsearch-head
-/opt/elasticsearch/bin/elasticsearch -f
+/opt/elasticsearch/bin/elasticsearch -f /opt/elasticsearch/config/config.yml
